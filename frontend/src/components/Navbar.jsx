bd3b78e1c6d7499532f8d80f3735c9b3709e025e
kenway974 <kpignolet18@gmail.com>
2025-11-06 21:25:37 +0100

    affichage react-cra ok

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./Navbar.css"; // Styles séparés
+
+
+function MyButton() {
+  return (
+    <button>Je suis un bouton</button>
+  );
+}
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      {/* Home */}
+      <Link to="/" className="home">
+        Home
+      </Link>
+
+      {/* Mobile burger */}
+      <button className="burger" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      {/* Menu items */}
+      <div className={`menu ${isOpen ? "open" : ""}`}>
+        <NavLink to="/trips" className="nav-item">
+          Trips
+        </NavLink>
+        <NavLink to="/contact" className="nav-item">
+          Contact
+        </NavLink>
+      </div>
+
+      {/* Connexion */}
+      <Link to="/login" className="login">
+        Connexion
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+
+function AppRouter() {
+  return (
+    <Routes>
+    </Routes>
+  );
+}
+
+export default AppRouter;
