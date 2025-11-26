5bc266543b77c7c62cf7c266f3aae76933f70298
kenway974 <kpignolet18@gmail.com>
2025-11-27 00:10:06 +0100

    axiosclient corrigé en https

--- frontend/src/AxiosClient.js
+++ frontend/src/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://ecoride-9r0a:9000/api",
+  baseURL: "https://ecoride-9r0a:9000/api",
   headers: {
     "Content-Type": "application/json",
   },
