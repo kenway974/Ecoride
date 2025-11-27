c6a297e545bb9342ecebcbceabaa331de3a3f7f5
kenway974 <kpignolet18@gmail.com>
2025-11-27 19:31:01 +0100

    nouveau url back ajouté a axios

--- frontend/src/AxiosClient.js
+++ frontend/src/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://ecoride-9r0a:9000/api",
+  baseURL: "https://ecoride-backend-qtb1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
